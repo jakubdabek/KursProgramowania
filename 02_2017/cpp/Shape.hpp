61c10c48d9c5bd2dd921ff9034d74ba975b37dc3
Jakub Dąbek <jakub.dabek@gmail.com>
2018-03-25 19:42:26 +0200

    Fix undefined behaviour in 03/02_2017
    Deleting a polymorphic object without a virtual destructor
    through a base class pointer is undefined behaviour
    This wasn't visible because of the use of std::unique_ptr
    P.S. std::shared_ptr doesn't suffer from this,
    because of the different behaviour of deleters

--- 02_2017/cpp/Shape.hpp
+++ 02_2017/cpp/Shape.hpp
@@ -6,4 +6,6 @@ class Shape
     virtual const char* name() const noexcept { return "Shape"; }
     virtual double surface_area() const noexcept = 0;
     virtual double perimeter() const noexcept = 0;
+
+    virtual ~Shape() = default;
 };
